2a6b7ca0c1c6c61879016e5979aed2d8c29d3bf8
Pedro <2022302914@aluno.erechim.ifrs.edu.br>
2025-11-20 17:37:07 -0300

    feat: Creating model and serializer Post

--- teste/teste_api/models.py
+++ teste/teste_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField
+    description = models.TextField
+    created = models.DateTimeField(auto_now_add=True)
+    last_edit = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+        )
+
+    class Meta:
+        ordering = ["created", "last_edit"]
--- teste/teste_api/serializers.py
+++ teste/teste_api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from teste.teste_api.models import Post
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["url", "title", "description", "created", "last_edit", "author"]
